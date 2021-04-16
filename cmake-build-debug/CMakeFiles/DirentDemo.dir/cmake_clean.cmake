d94221c6d0ffc7d518103f332ab6acfc977c5f83
Micklerick617 <mlennon@smu.edu>
2021-04-15 21:06:47 -0500

    Dirent demo commit

---
+++ cmake-build-debug/CMakeFiles/DirentDemo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DirentDemo.dir/main.cpp.obj"
+  "DirentDemo.exe"
+  "DirentDemo.exe.manifest"
+  "DirentDemo.pdb"
+  "libDirentDemo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DirentDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+#include <dirent.h> // Include this header file to read directories! (Note the '<>' as opposed to '""')
+
+/*******************************************************************
+ * CS 2341 - Demo project for reading in files from a directory.
+ * Done using Dirent.h.
+ *
+ * argv[1] - The absolute path to the directory we want to read.
+ *******************************************************************/
+
+/**
+ * main function.
+ *
+ * @param argc -    number of command line arguments.
+ * @param argv -    char** containing each command line argument.
+ *                  argv[1] - the absolute path to the directory we want to read.
+ *                      For the demo, set it to "path_to_project/DirentDemo/DirectoryToRead"
+ *
+ * @return 0 if directory was successfully accessed read, 1 if not.
+ */
+int main(int argc, char** argv) {
+    struct dirent *entry = nullptr; // A dirent object, which stores the contents of a file in a directory.
+    DIR *dp = nullptr;              // A DIR object. Think of it as a 'directory iterator' -- it reads through all the files in a directory.
+
+    // Set our 'dir' variable to the absolute directory path.
+    std::string dir = argv[1];
+
+    // This will be in the form "<directory>/<current_file>".
+    std::string thisFilePath = "";
+
+    dp = opendir(dir.c_str());  // Sets dp to the first file in the directory with path 'dir.'
+                                // Think of this as a 'begin()' function for a standard sequential container iterator (std::vector iterator, etc.).
+
+    // If the DIR object (directory iterator) successfully opened a directory, iterate through all files using readdir() in a while loop.
+    if (dp != nullptr) {
+        while ((entry = readdir(dp))) {
+            thisFilePath = dir + "/" + entry->d_name; // This will set 'thisFilePath' to the ABSOLUTE path of the FILE.
+
+            // Often times, directories read include '.' (current directory) and '..' (previous directory).
+            // In our implementation, we're ignoring them.
+            if (thisFilePath.find_last_of(".") == thisFilePath.size() - 1 || thisFilePath.find_last_of("..") == thisFilePath.size() - 1 ) continue;
+            // '.' - current directory. C:\Users\mplen\ta-code\DirentDemo\DirectoryToRead
+            // '..' - C:\Users\mplen\ta-code\DirentDemo
+
+
+            // Open up the current file in the directory (absolute path) to read its contents.
+            std::ifstream fin(thisFilePath.c_str());
+
+            std::string lineToPrintOut;
+            // Reads in the first line of each file.
+            getline(fin, lineToPrintOut);
+
+            // Print out contents of the line just read.
+            std::cout << lineToPrintOut << std::endl;
+        }
+    }
+    else {
+        std::cout << "DirentDemo - main(): Directory not found" << std::endl;
+        return 1;
+    }
+
+    // Close the directory when done.
+    closedir(dp);
+
+    return 0;
+}
